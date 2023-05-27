0b31a47cc4377d59117a599d8cde508ff9347263
Swapnil Shah <swapnilshah5889@gmail.com>
2023-05-27 15:35:37 -0400

    added monster's api to fetch all monsters and an input tag that filters the monsters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    monsters:[],
+    searchField:''
+  });
+
+
+  useEffect(() => {
+    console.log('Component mounted');
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const jsonData = await response.json();
+        setState(prevState => ({ ...prevState, monsters: jsonData }));
+        filteredMonsters = state.monsters;
+      } catch (error) {
+        console.log("Error fetching the api")
+      }
+    }
+    
+    fetchData();
+  }, [])
+  
+  useEffect(() => {
+    if(state.monsters) {
+      console.log('Response : ', state.monsters);
+    }
+  }, [state.monsters])
+  
+  const filteredMonsters = state.monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(state.searchField);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('Render')}
+
+      {/* Input field */}
+      <input className='search-box' type='search' placeholder='Search Monster' onChange={(event)=>{
+        console.log(event);
+        const searchString = event.target.value.toLocaleLowerCase();
+        setState(prevState => ({ ...prevState, searchField: searchString} ));
+      }}/>
+
+      {/* List of Monsters */}
+      {filteredMonsters.map((monster) => (
+        <div key={monster.id}>
+          <p>{monster.name}</p>
+        </div>
+      ))}
+
     </div>
   );
 }
