154eba08961d63c38f8f55273bb69acd4cda6e51
Swapnil Shah <swapnilshah5889@gmail.com>
2023-05-27 16:00:23 -0400

    added comments

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ function App() {
   useEffect(() => {
     console.log('Component mounted');
     
+    // Asynchoronous call to fetch data from API
     const fetchData = async () => {
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const jsonData = await response.json();
+        // Merge state objects
         setState(prevState => ({ ...prevState, monsters: jsonData }));
       } catch (error) {
         console.log("Error fetching the api")
@@ -24,27 +26,40 @@ function App() {
     }
     
     fetchData();
-  }, [])
+  }, 
+  // The useEffect array is empty so it will be called only once
+  []) 
   
   useEffect(() => {
     if(state.monsters) {
       console.log('Response : ', state.monsters);
     }
-  }, [state.monsters])
+  }, 
+  // The useEffect array has state.monsters so this 
+  // useEffect will be triggered everytime state.monsters is updated
+  [state.monsters]) 
   
+
+  // Filtered monsters will be set everytime monsters or searchfield changes in the state
   useEffect(() => {
+    // Filter monsters from state.monsters and then set it to a variable filteredMonsters
     const filteredMonsters = state.monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(state.searchField);
     });
+    // Set filteredMonsters using state
     setFilteredMonsters(filteredMonsters);
-  }, [state.monsters, state.searchField]);
+  },
+  // The useEffect array has state.monsters & state.searchField so this 
+  // useEffect will be triggered everytime state.monsters or state.searchField is updated
+  [state.monsters, state.searchField]);
   
   const [filteredMonsters, setFilteredMonsters] = useState([]);
 
-
+  // Handle On search change event : fetch value from input field and set it in the state
   const onSearchChanged = (event) =>{
     console.log(event);
     const searchString = event.target.value.toLocaleLowerCase();
+    // using prevState we preserve the old state object and merge the changes in the 2nd parameter
     setState(prevState => ({ ...prevState, searchField: searchString} ));
   };
 
