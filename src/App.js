083c354d5be6e388c9baa2f92a1ce71fcdea42d5
Swapnil Shah <swapnilshah5889@gmail.com>
2023-06-23 12:54:45 -0400

    Added font family for app title

--- src/App.js
+++ src/App.js
@@ -77,7 +77,8 @@ function App() {
     <div className="App">
 
       {/* App Title */}
-      <h1 className='app-title'>Monster Diary</h1>
+      <h1 className='app-title'>Monster's Diary</h1>
+
       {/* Input field */}
       <SearchBox 
         className={'monsters-search-box'}
